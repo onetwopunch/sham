cab5bf3c0d4dea199839c67112bdc7086d5b0751
Ryan <ryan@scalesec.com>
2018-12-12 11:26:10 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# `sham` - Shamir secret sharing for everyone
+
+Brings the Shamir secret sharing algorithm from Hashicorp Vault to a CLI utility for usage outside Vault. Shamir breaks a secret key into shards such that a certain threshold is required to re-assemble the keys. This allows splitting a key into cryptographically isolated keys where no one key gives information about the final secret.
+
+## Installation
+
+```
+go get github.com/onetwopunch/sham
+```
+
+## Usage
+
+```
+$ ./sham --help
+Usage of ./sham:
+  -combine string
+    	The shards to combine to get the secret separated by commas
+  -k int
+    	The number of total key shards (default 3)
+  -split string
+    	The secret to split into shards
+  -t int
+    	The min number of shards needed to re-assemble the secret (default 2)
+```
+## Examples
+
+### Splitting a secret key
+```
+$ sham --split 'supersecret' -k 4 -t 3
+hvXh_5lEQ7-4f79f
+Ee0n0a6bGBu7UesJ
+N4WJV4j11Ys9WuVE
+SGPS5nQMmNeg-Nnb
+```
+
+### Combining a secret key
+```
+$ ./sham --combine hvXh_5lEQ7-4f79f,Ee0n0a6bGBu7UesJ,N4WJV4j11Ys9WuVE
+supersecret
+```
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+  "encoding/base64"
+  "fmt"
+  "github.com/hashicorp/vault/shamir"
+  "log"
+  "os"
+  "strings"
+  "flag"
+)
+
+func handleError(err error) {
+  if err != nil {
+    log.Fatalln(err)
+  }
+}
+
+var encoder *base64.Encoding = base64.URLEncoding
+var secret, shardsCommaSep string
+var k, t int
+
+func init() {
+  flag.StringVar(&secret, "split", "", "The secret to split into shards")
+  flag.StringVar(&shardsCommaSep, "combine", "", "The shards to combine to get the secret separated by commas")
+  flag.IntVar(&k, "k", 3, "The number of total key shards")
+  flag.IntVar(&t, "t", 2, "The min number of shards needed to re-assemble the secret")
+  flag.Parse()
+}
+
+func main() {
+  if len(secret) > 0 && len(shardsCommaSep) > 0 {
+    fmt.Println("ERROR: Cannot both split and combine in the same action")
+    flag.Usage()
+    os.Exit(1)
+  } else if len(secret) == 0 && len(shardsCommaSep) == 0 {
+    flag.Usage()
+    os.Exit(1)
+  }
+
+  // Split Command selected
+  if len(secret) > 0 {
+    keys, err := shamir.Split([]byte(secret), k, t)
+    if err != nil {
+      log.Fatalln(err)
+    }
+
+    for _, shard := range keys {
+      // RFC 4648 Encoding
+      encoded := encoder.EncodeToString(shard)
+      fmt.Println(encoded)
+    }
+  } else {
+    shards := strings.Split(shardsCommaSep, ",")
+    shardBytes := make([][]byte, len(shards))
+    for i, shard := range shards {
+      decodedShard, err := base64.URLEncoding.DecodeString(shard)
+      handleError(err)
+      shardBytes[i] = []byte(decodedShard)
+    }
+
+    decodedSecret, err := shamir.Combine(shardBytes)
+    handleError(err)
+    fmt.Println(decodedSecret)
+  }
+}
